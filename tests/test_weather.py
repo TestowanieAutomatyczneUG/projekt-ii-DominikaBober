bed971f07f06b914e423833153288885a279729a
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2022-01-23 14:33:28 +0100

    write weather_static tests

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -3,15 +3,20 @@ sys.path.append("./")
 from src.Weather import Weather
 from packages import *
 
+from matchers import is_good_Describtion_values, is_good_Describtion_keys
+from src.config import host
+
+assertpy.add_extension(is_good_Describtion_keys)
+
 data_meteo = pd.read_csv("data/data_meteo_2008_03-07.csv")
 data_meteo["datetime"] = pd.to_datetime(data_meteo["datetime"])
 
 class Test_Weather(unittest.TestCase):
 
-    testing = "all"
+    testing = "test_weather_static"
 
     def setUp(self):
-        self.serwer = Weather()
+        self.serwer = Weather(host)
 
     @parameterized.parameterized.expand(
         list(itertools.chain.from_iterable(
@@ -54,5 +59,78 @@ class Test_Weather(unittest.TestCase):
         self.serwer.get_data = mock.Mock(name = "get_data")
         self.serwer.get_data.return_value = temp
         self.assertTrue(list(temp['datetime'])[-1].date() + datetime.timedelta(days=1) == list(self.serwer.get_weather_forecast(site, days)['datetime'])[0].date())
+
+    @parameterized.parameterized.expand(
+        list(map(lambda site:
+                (site), 
+        list(data_meteo.columns)[1:]))
+    )
+    @unittest.skipIf(testing != "test_weather_static" and testing != "all", "TDD")
+    def test_weather_static_length(self, site):
+        temp = data_meteo[["datetime", site]]
+        temp.columns = ["datetime", "value"]
+        self.serwer.get_localizations = mock.Mock(name = "get_localizations")
+        self.serwer.get_localizations.return_value = list(data_meteo.columns)[1:]
+        self.serwer.get_data = mock.Mock(name = "get_data")
+        self.serwer.get_data.return_value = temp
+        self.assertTrue(len(self.serwer.weather_static(site).index) == 8)
+
+    @parameterized.parameterized.expand(
+        list(map(lambda site:
+                (site), 
+        list(data_meteo.columns)[1:]))
+    )
+    @unittest.skipIf(testing != "test_weather_static" and testing != "all", "TDD")
+    def test_weather_static_values(self, site):
+        temp = data_meteo[["datetime", site]]
+        temp.columns = ["datetime", "value"]
+        self.serwer.get_localizations = mock.Mock(name = "get_localizations")
+        self.serwer.get_localizations.return_value = list(data_meteo.columns)[1:]
+        self.serwer.get_data = mock.Mock(name = "get_data")
+        self.serwer.get_data.return_value = temp
+        hamcrest.assert_that([self.serwer.weather_static(site)['value'], temp.describe()['value']], hamcrest.is_(is_good_Describtion_values()))
+    
+    @parameterized.parameterized.expand(
+        list(map(lambda site:
+                (site), 
+        list(data_meteo.columns)[1:]))
+    )
+    @unittest.skipIf(testing != "test_weather_static" and testing != "all", "TDD")
+    def test_weather_static_keys(self, site):
+        temp = data_meteo[["datetime", site]]
+        temp.columns = ["datetime", "value"]
+        self.serwer.get_localizations = mock.Mock(name = "get_localizations")
+        self.serwer.get_localizations.return_value = list(data_meteo.columns)[1:]
+        self.serwer.get_data = mock.Mock(name = "get_data")
+        self.serwer.get_data.return_value = temp
+        assertpy.assert_that(self.serwer.weather_static(site).index).is_good_Describtion_keys()
+    
+    @parameterized.parameterized.expand(
+        list(map(lambda site:
+                (site), 
+        list(data_meteo.columns)[1:]))
+    )
+    @unittest.skipIf(testing != "test_weather_static" and testing != "all", "TDD")
+    def test_weather_static_localizations(self, site):
+        temp = data_meteo[["datetime", site]]
+        temp.columns = ["datetime", "value"]
+        self.serwer.get_localizations = mock.Mock(name = "get_localizations")
+        self.serwer.get_localizations.return_value = list(data_meteo.columns)[1:]
+        self.serwer.get_data = mock.Mock(name = "get_data")
+        self.serwer.get_data.return_value = temp
+        self.serwer.weather_static(site)
+
+    @parameterized.parameterized.expand([
+        ('', ),
+        ('1234', ),
+        ('56789', ),
+        (123, )
+    ])
+    @unittest.skipIf(testing != "test_weather_static" and testing != "all", "TDD")
+    def test_weather_static_localizations_exceptions(self, site):
+        self.serwer.get_localizations = mock.Mock(name = "get_localizations")
+        self.serwer.get_localizations.return_value = list(data_meteo.columns)[1:]
+        assertpy.assert_that(self.serwer.weather_static).raises(Exception).when_called_with(site)
+
 
 unittest.main()
